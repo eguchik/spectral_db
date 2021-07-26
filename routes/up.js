31d28ec425a7c301f71cb4b3b92c24461f611bc2
eguchik <k.eguchi84@gmail.com>
2021-07-26 18:37:49 +0900

    disk storage engineの利用

--- routes/up.js
+++ routes/up.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var path = require('path')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './static')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname))
+    }
+  })
+  
+var upload = multer({ storage: storage })
+
 
 router.get('/', (req, res) => {
   res.render('up');
 })
 
 router.post('/',
-  multer({dest: './uploads/'}).single('file'),
-  (req, res) => {
-    // multerが/tmp/samplefup/配下にファイルを作成
+  upload.single('file'), (req, res) => {
+    // single('file')のfileはform要素のname属性と一致
 
     // req.file.pathでmulterが作成したファイルのパスを取得可能
     console.log(req.file.path, req.file.originalname);
@@ -18,3 +30,5 @@ router.post('/',
 
 
 module.exports = router;
+
+
